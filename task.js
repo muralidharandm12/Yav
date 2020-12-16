ec2f06c814beb8ce01c6e2b2b1c9493b335f7cd7
muralidharandm12 <75481651+muralidharandm12@users.noreply.github.com>
2020-12-16 15:29:32 +0530

    postgetdelput

--- task.js
+++ task.js
@@ -1,49 +1,70 @@
+const apiendpoint = "https://crudcrud.com/api/741836c546c24e9e86f006e13a1c13d3";
 
+
+document.addEventListener("DOMContentLoaded", () => {  
+  localStorage.getItem();
+});
 
 if (document.readyState !== "loading") {
-    console.log("vscode");
- 
-    var keys = Object.keys(localStorage), //taking out all the keys that are there in the local storage
-      i = keys.length; //6
-      console.log('keys', keys)
-      let stringifiedDetails, details;
- // 6 to 0
-    while (i--) { //i==2
-      if (keys[i].match(/userDetails/g)) {
-        //we only care about keys that start with userDetails
-        //this is called regex matching
-      stringifiedDetails = localStorage.getItem(keys[i]);
-        console.log("stringifiedDetailsOfPeople", stringifiedDetails);
-        details = JSON.parse(stringifiedDetails);
-        console.log("details", details);
-       addNewLineElement(details);
+  async function getUserData() {    
+    await axios.get(`${apiendpoint}/registeruser`).then((data) => {
+      
+      for (let i = 0; i < data.data.length; i++) {
+        addNewLineElement(data.data[i]);
       }
-    }
-  }
+      });
+      }
+
+      getUserData();
+}
+
+
 submit.onclick = function(){
     const name = document.getElementById("name").value;
     const email = document.getElementById("email").value ;
     const pnumber = document.getElementById("pnumber").value; 
-    const object = {
+    if (email.length > 0 && name.length > 0 && pnumber.length>0) {
+      const object = {
         name: name,
         email: email,
         pnumber:pnumber
       };
-
-    if(name && email && pnumber)
-    {
-        localStorage.setItem("userDetails"+email, JSON.stringify(object));
-        
-    }
-    
+      
+      axios.post(`${apiendpoint}/registeruser`, object); 
+          
     addNewLineElement(object);
+
+    }
 };
 
 
 function addNewLineElement(object) {
     const ul = document.getElementById("listOfPeople");
     const li = document.createElement("li");
-    li.appendChild(document.createTextNode(object.name + " " + object.email+ " " + object.pnumber));
+    li.appendChild(
+      document.createTextNode(object.name + " " + object.email+ " " + object.pnumber)
+    );
     ul.appendChild(li);
-    
+
+    const deletebutton = document.createElement("input");
+  deletebutton.type = "button";
+  deletebutton.value = "Delete";
+  deletebutton.addEventListener("click", () => {
+    axios.delete(`${apiendpoint}/registeruser/${object._id}`);
+  });
+  deletebutton.className = "delete";
+  deletebutton.style.border = "2px solid red";  
+  li.appendChild(deletebutton);
+  ul.appendChild(li);
+
+  const updatebutton = document.createElement("input");
+  updatebutton.type = "button";
+  updatebutton.value = "Update";
+  updatebutton.addEventListener("click", () =>{
+    axios.put(`${apiendpoint}/registeruser/${object.name,object.email,object.pnumber}`);
+  });
+updatebutton.className = "edit";
+updatebutton.style.border = "2px solid red";
+li.appendChild(updatebutton);
+ul.appendChild(li);
   }
